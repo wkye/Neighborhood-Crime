77d036b89d771b68e4e4cbbbd4dcc6de0a48048c
wkye <wkye@nd.edu>
2016-12-14 10:39:04 -0500

    readme

---
+++ Readme.R
@@ -0,0 +1,4 @@
+#Readme
+
+#Project examining the role of civic organizations in perpetuating or deterring crime in neighborhoods
+# and how these effects are hetergenous across cities and neighborhood demographics
